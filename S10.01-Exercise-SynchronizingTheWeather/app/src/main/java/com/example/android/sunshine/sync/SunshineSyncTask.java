4f3bdb2af6702d5b31d9ad9f62f81f355b8ec01a
Ramiz Raja <informramiz@gmail.com>
2018-09-11 14:13:58 +0500

    feat: Sync data with IntentService

--- S10.01-Exercise-SynchronizingTheWeather/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncTask.java
+++ S10.01-Exercise-SynchronizingTheWeather/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncTask.java
@@ -1,4 +1,55 @@
-//  TODO (1) Create a class called SunshineSyncTask
-//  TODO (2) Within SunshineSyncTask, create a synchronized public static void method called syncWeather
-//      TODO (3) Within syncWeather, fetch new weather data
-//      TODO (4) If we have valid results, delete the old data and insert the new
+package com.example.android.sunshine.sync;
+
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.support.annotation.WorkerThread;
+
+import com.example.android.sunshine.data.WeatherContract;
+import com.example.android.sunshine.utilities.NetworkUtils;
+import com.example.android.sunshine.utilities.OpenWeatherJsonUtils;
+import com.example.android.sunshine.utilities.SunshineWeatherUtils;
+
+import java.net.URL;
+
+//  COMPLETED (1) Create a class called SunshineSyncTask
+//  COMPLETED (2) Within SunshineSyncTask, create a synchronized public static void method called syncWeather
+//      COMPLETED (3) Within syncWeather, fetch new weather data
+//      COMPLETED (4) If we have valid results, delete the old data and insert the new
+public final class SunshineSyncTask {
+    @WorkerThread
+    synchronized public static void syncWeather(Context context) {
+        try {
+            final URL weatherRequestUrl = NetworkUtils.getUrl(context);
+            String jsonWeatherResponse = NetworkUtils.getResponseFromHttpUrl(weatherRequestUrl);
+
+            final ContentValues[] weatherValues = OpenWeatherJsonUtils
+                    .getWeatherContentValuesFromJson(context, jsonWeatherResponse);
+            if (isDataValid(weatherValues)) {
+                deleteAllData(context);
+                insertData(context, weatherValues);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static boolean isDataValid(@Nullable final ContentValues[] weatherValues) {
+        return weatherValues != null && weatherValues.length > 0;
+    }
+
+    private static void deleteAllData(Context context) {
+        final ContentResolver contentResolver = context.getContentResolver();
+        contentResolver.delete(
+                WeatherContract.WeatherEntry.CONTENT_URI,
+                null,
+                null
+        );
+    }
+
+    private static void insertData(Context context, final ContentValues[] contentValues) {
+        final ContentResolver contentResolver = context.getContentResolver();
+        contentResolver.bulkInsert(WeatherContract.WeatherEntry.CONTENT_URI, contentValues);
+    }
+}
--- S10.01-Exercise-SynchronizingTheWeather/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncUtils.java
+++ S10.01-Exercise-SynchronizingTheWeather/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncUtils.java
@@ -1,3 +1,16 @@
-// TODO (9) Create a class called SunshineSyncUtils
-    //  TODO (10) Create a public static void method called startImmediateSync
-    //  TODO (11) Within that method, start the SunshineSyncIntentService
+package com.example.android.sunshine.sync;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.AnyThread;
+
+// COMPLETED (9) Create a class called SunshineSyncUtils
+    //  COMPLETED (10) Create a public static void method called startImmediateSync
+    //  COMPLETED (11) Within that method, start the SunshineSyncIntentService
+public final class SunshineSyncUtils {
+    @AnyThread
+    public static void startImmediateSync(Context context) {
+        Intent startSyncServiceIntent = new Intent(context, SunshineSyncIntentService.class);
+        context.startService(startSyncServiceIntent);
+    }
+}
